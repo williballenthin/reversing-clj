3b5fd26a37897fe9d426575a7c7c63e24b84df99
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-05-30 10:40:46 -0400

    capstone: reformat test

--- capstone-clj/test/capstone_clj/core_test.clj
+++ capstone-clj/test/capstone_clj/core_test.clj
@@ -4,7 +4,6 @@
   (:import [capstone.Capstone]))
 
 
-;;(import capstone.Capstone)
 (deftest basic-capstone
   "
     this is the example from:
@@ -16,27 +15,27 @@
           flavor capstone.Capstone/CS_OPT_SYNTAX_INTEL
           cs (capstone.Capstone. arch mode)
           _ (.setSyntax cs flavor)
-          _ (.setDetail cs 1)]
-      (let [code (byte-array [0x55
-                              0x48
-                              0x8b
-                              0x05
-                              0xb8
-                              0x13
-                              0x00
-                              0x00])
-            insns (.disasm cs code 0x1000)]
-        (testing "disassemble"
-          (is (= (alength insns) 2))
-          (doseq [[i insn] (map-indexed vector insns)]
-            (let [addr (.-address insn)
-                  mnem (.-mnemonic insn)
-                  op   (.-opStr insn)]
-              (printf "0x%x:\t%s\t%s\n" addr mnem op)
-              (condp = i
-                0 (testing "first opcode"
-                    (is (= addr 0x1000))
-                    (is (= mnem "push")))
-                1 (testing "second opcode"
-                    (is (= addr 0x1001))
-                    (is (= mnem "mov")))))))))))
+          _ (.setDetail cs 1)
+          code (byte-array [0x55
+                            0x48
+                            0x8b
+                            0x05
+                            0xb8
+                            0x13
+                            0x00
+                            0x00])
+          insns (.disasm cs code 0x1000)]
+      (testing "disassemble"
+        (is (= (alength insns) 2))
+        (doseq [[i insn] (map-indexed vector insns)]
+          (let [addr (.-address insn)
+                mnem (.-mnemonic insn)
+                op   (.-opStr insn)]
+            (printf "0x%x:\t%s\t%s\n" addr mnem op)
+            (condp = i
+              0 (testing "first opcode"
+                  (is (= addr 0x1000))
+                  (is (= mnem "push")))
+              1 (testing "second opcode"
+                  (is (= addr 0x1001))
+                  (is (= mnem "mov")))))))))))
