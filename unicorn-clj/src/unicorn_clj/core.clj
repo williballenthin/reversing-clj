977d2fb6bd6cdd4625cb59255e614e09b58707e1
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-05-30 12:33:44 -0400

    unicorn: add clojure-style wrapper

--- unicorn-clj/src/unicorn_clj/core.clj
+++ unicorn-clj/src/unicorn_clj/core.clj
@@ -1,7 +1,77 @@
 (ns unicorn-clj.core
+  (:import [clojure.lang ILookup]
+           [clojure.string]
+           [unicorn.Unicorn])
   (:gen-class))
+
+
+(defn mem_map!
+  ([emu addr length perms]
+   (.mem_map (.uc emu) addr length perms))
+  ([emu addr length]
+   (mem_map! emu addr length unicorn.Unicorn/UC_PROT_EXEC)))
+
+(defn mem_write!
+  [emu addr buf]
+  (.mem_write (.uc emu) addr buf))
+
+(defn mem_read
+  [emu addr addr length]
+  (.mem_read (.uc emu) addr length))
+
+(defn emu_start!
+  ([emu addr until timeout count]
+   (.emu_start (.uc emu) addr until timeout count))
+  ([emu addr until]
+   (emu_start! emu addr until 0 0)))
+
+(defn- resolve-reg
+  [emu key]
+  (when (= (.arch emu) unicorn.Unicorn/UC_ARCH_X86)
+    (let [regname (clojure.string/upper-case (subs (str key) 1))
+          uname (str "UC_X86_REG_" regname)
+          ;; well, this is a hack.
+          ;; access to static fields must be compiled ahead of time.
+          ;; so we generate code to fetch the requested register.
+          ;; ref: https://stackoverflow.com/questions/6630432/access-java-fields-dynamically-in-clojure
+          ;;
+          ;; TODO: cache the generated code?
+          reg (eval (read-string (str "unicorn.Unicorn/" uname)))]
+      reg)))
+
+(defn reg_write!
+  [emu reg val]
+  (if (keyword? reg)
+    (.reg_write (.uc emu) (resolve-reg emu reg) val)
+    (.reg_write (.uc emu) reg val)))
+
+(defn reg_read
+  [emu reg]
+  (let [reg' (if (keyword? reg)
+               (resolve-reg emu reg)
+               reg)
+        val (.reg_read (.uc emu) reg')]
+    ;; note: return from reg_read is always 64bit
+    (if (= unicorn.Unicorn/UC_MODE_32)
+      (bit-and 0xFFFFFFFF val)
+      val)))
+
+(deftype Emulator [uc arch mode]
+  ILookup
+  (valAt [this key]
+     (reg_read this (resolve-reg this key))))
+
+(defn make-emulator
+  ([arch mode]
+   (Emulator. (unicorn.Unicorn. arch mode) arch mode)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (let [arch unicorn.Unicorn/UC_ARCH_X86
+        mode unicorn.Unicorn/UC_MODE_32
+        uc (unicorn.Unicorn. arch mode)]
+    (println "a" (:eax uc)))
+  (println "Goodbye, World!"))
+
--- unicorn-clj/test/unicorn_clj/core_test.clj
+++ unicorn-clj/test/unicorn_clj/core_test.clj
@@ -1,7 +1,8 @@
 (ns unicorn-clj.core-test
   (:require [clojure.test :refer :all]
             [unicorn-clj.core :refer :all])
-  (:import [unicorn.Unicorn]))
+  (:import [unicorn.Unicorn]
+           [clojure.lang ILookup]))
 
 
 (deftest basic-unicorn
@@ -9,7 +10,7 @@
     this is the example from:
     http://www.unicorn-engine.org/docs/tutorial.html
   "
-  (testing "basic unicorn"
+  (testing "raw unicorn"
     (let [arch unicorn.Unicorn/UC_ARCH_X86
           mode unicorn.Unicorn/UC_MODE_32
           mu (unicorn.Unicorn. arch mode)
@@ -24,9 +25,30 @@
       ;; note: return from reg_read is always 64bit
       (let [ecx (bit-and 0xFFFFFFFF (.reg_read mu unicorn.Unicorn/UC_X86_REG_ECX))
             edx (bit-and 0xFFFFFFFF (.reg_read mu unicorn.Unicorn/UC_X86_REG_EDX))]
-        (printf "ecx: 0x%x\n" ecx)
-        (printf "edx: 0x%x\n" edx)
         (testing "emulation"
           (is (= ecx 0x1235))
           (is (= edx 0x788f)))))))
 
+(deftest unicorn-clj
+  (testing "unicorn-clj"
+    (let [arch unicorn.Unicorn/UC_ARCH_X86
+          mode unicorn.Unicorn/UC_MODE_32
+          mu (make-emulator arch mode)
+          code (byte-array [0x41 0x4a])
+          addr 0x1000000]
+      (doto mu
+        (mem_map! addr (* 2 1024 1024) unicorn.Unicorn/UC_PROT_EXEC)
+        (mem_write! addr code)
+        ;; here are two equivalent ways to set register contents:
+        (reg_write! unicorn.Unicorn/UC_X86_REG_ECX 0x1234)
+        (reg_write! :edx 0x7890)
+        ;; default timeout, count values are 0
+        (emu_start! addr (+ addr (alength code))))
+      ;; here are two equivalent ways to fetch register contents:
+      (testing "reg read"
+        ;; note: in MODE_32, result is automatically 32 bits.
+        (is (= (reg_read mu unicorn.Unicorn/UC_X86_REG_ECX) 0x1235))
+        (is (= (reg_read mu unicorn.Unicorn/UC_X86_REG_EDX) 0x788f)))
+      (testing "reg lookup"
+        (is (= (:ecx mu) 0x1235))
+        (is (= (:edx mu) 0x788f))))))
