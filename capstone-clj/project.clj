19c4c4293693e13f801bfb5157cc64dd9f478c7e
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-09-02 13:07:18 -0400

    capstone: add details on installing capstone deps

--- capstone-clj/project.clj
+++ capstone-clj/project.clj
@@ -1,11 +1,23 @@
-(defproject capstone-clj "0.1.0-SNAPSHOT"
+(defproject com.williballenthin/capstone-clj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.java.dev.jna/jna "4.1.0"]]
-  :resource-paths ["resources/capstone.jar"]
+  ;; to install capstone for clojure:
+  ;;    $ git clone https://github.com/aquynh/capstone.git
+  ;;    $ cd capstone
+  ;;    $ ./make.sh
+  ;;    $ cd bindings/java
+  ;;    $ sudo dnf install jna              # fedora
+  ;;    $ sudo apt-get install libjna-java  # ubuntu
+  ;;    $ make
+  ;;    $ mvn install:install-file -Dfile=$(pwd)/capstone.jar
+  ;;                               -DgroupId=capstone
+  ;;                               -DartifactId=capstone.jar
+  ;;                               -Dversion=3.5.0-rc3
+  ;;                               -Dpackaging=jar
   :main ^:skip-aot capstone-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
