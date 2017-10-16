885cda9a54216b90d3dbb6a1bfdf8312ed67b1fd
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-10-16 02:52:10 -0400

    analysis: remove unreferenced index-by

--- lancelot-clj/src/lancelot_clj/anal.clj
+++ lancelot-clj/src/lancelot_clj/anal.clj
@@ -5,21 +5,6 @@
   (:import [capstone.X86_const]))
 
 
-(defn index-by
-  "
-  create a map indexed by the given key of the given collection.
-  like `group-by`, except its assumed there's only one value per key.
-
-  example::
-
-      (index-by [{:a 1 :b 2} {:a 3 :b 4}] :a)
-      => {1 {:a 1 :b 2}
-          3 {:a 3 :b 4}}
-  "
-  [col k]
-  (into {} (map #(vector (get % k) %) col)))
-
-
 (defn conj-if [c e]
   (if (not (nil? e))
     (conj c e)
