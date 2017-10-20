d45f1e5542378837173f9d5fd46762d1cea3caea
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-10-20 09:57:36 -0400

    project: add deps for logging

--- lancelot-clj/project.clj
+++ lancelot-clj/project.clj
@@ -9,7 +9,10 @@
                  [capstone/capstone "3.5.0-rc3"]
                  [com.williballenthin/pe "0.1.0-SNAPSHOT"]
                  ;; for mime-type detection. pulls in lots of stuff. ripe for replacement.
-                 [com.novemberain/pantomime "2.9.0"]]
+                 [com.novemberain/pantomime "2.9.0"]
+                 ;; for logging
+                 [org.clojure/tools.logging "0.4.0"]
+                 [ch.qos.logback/logback-classic "1.1.3"]]
   :main ^:skip-aot lancelot-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
