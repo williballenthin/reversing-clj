68aa1640f32ab1983824796a644e5bc9f2c8ab1a
Willi Ballenthin <willi.ballenthin@gmail.com>
2017-10-20 10:28:51 -0400

    testutils: enable switching of dis detail mode

--- lancelot-clj/test/lancelot_clj/testutils.clj
+++ lancelot-clj/test/lancelot_clj/testutils.clj
@@ -14,12 +14,21 @@
     byte-buffer))
 
 
+;; via: https://stackoverflow.com/a/29587909
+(defn boolean? [x]
+  (instance? Boolean x))
+
+
 (defn make-capstone
-  [arch mode]
-  (let [cs (capstone.Capstone. arch mode)
-        _ (.setSyntax cs capstone.Capstone/CS_OPT_SYNTAX_INTEL)
-        _ (.setDetail cs 1)]
-    cs))
+  ([arch mode detail]
+   (let [cs (capstone.Capstone. arch mode)
+         _ (.setSyntax cs capstone.Capstone/CS_OPT_SYNTAX_INTEL)
+         _ (.setDetail cs (if (boolean? detail)
+                            (if detail 1 0)
+                            detail))]
+     cs))
+  ([arch mode]
+   (make-capstone arch mode 1)))
 
 
 (defn format-insn
