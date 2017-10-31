8e30ba7abc7de097d24fb33d002dc818d442bf54
William Ballenthin <william.ballenthin@fireeye.com>
2017-10-31 17:34:39 -0400

    api: add graphql client helpers

--- lancelot-clj/src/lancelot_cljs/api.cljs
+++ lancelot-clj/src/lancelot_cljs/api.cljs
@@ -26,6 +26,20 @@
                         body))
      ch)))
 
+(defn http-post [url body s e]
+  (go
+    (let [resp (<! (http POST url body))]
+      (if (= :success (:status resp))
+        (s (:response resp))
+        (e (:response resp))))))
+
+(defn http-get [url s e]
+  (go
+    (let [resp (<! (http GET url))]
+      (if (= :success (:status resp))
+        (s (:response resp))
+        (e (:response resp))))))
+
 
 (defn- api-get [url s e]
   (go
@@ -61,5 +75,32 @@
   ;; get extended disassembly info for 10 instructions: aoj 10
   (http GET (api-url (str "/s " addr "; aoj " count))))
 
+(defn- http-graphql-post
+  "make a GraphQL query request to given endpoint with the given query.
+   invoke the given success handler on success.
+   invoke the given error handler on error.
+   this is a POST-based request.
+  "
+  [url query s e]
+  (POST url {:format          :json
+             :response-format :json
+             :keywords?       true
+             :params          {:query query}
+             :handler         s
+             :error-handler   e}))
 
+(defn- http-graphql-get
+  "make a GraphQL query request to given endpoint with the given query.
+   invoke the given success handler on success.
+   invoke the given error handler on error.
+   this is a GET-based request.
+  "
+  [url query s e]
+  (GET url {:format           :json
+            :response-format :json
+            :keywords?       true
+            :params          {:query query}
+            :handler         s
+            :error-handler   e}))
 
+(def http-graphql http-graphql-post)
